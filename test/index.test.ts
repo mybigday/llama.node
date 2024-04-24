fe6edbbd02e0fdf0447bdeac0a4fefb732a44bff
Hans <hans.chen@bricks.tools>
2024-04-24 11:28:37 +0800

    Change to tiny random

---
+++ test/index.test.ts
@@ -0,0 +1,15 @@
+import path from 'path'
+import { loadModel } from '../lib'
+
+it('work fine', async () => {
+  const model = loadModel({ model: path.resolve(__dirname, './tiny-random-llama.gguf') })
+  const result = await model.completion({
+    prompt: 'My name is Merve and my favorite',
+    n_samples: 1,
+    temperature: 0,
+    max_length: 100,
+    n_predict: 10,
+    seed: 0,
+  }, () => {})
+  expect(result).toMatchSnapshot()
+})
