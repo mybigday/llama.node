5e9ac9033dcb69e180758c05ce38e2e334058408
Hans <hans.chen@bricks.tools>
2024-05-01 03:27:18 +0800

    feat: trying support QNN

--- src/LlamaContext.cpp
+++ src/LlamaContext.cpp
@@ -57,6 +57,7 @@ LlamaContext::LlamaContext(const Napi::CallbackInfo &info)
   params.use_mmap = get_option<bool>(options, "use_mmap", true);
   params.numa =
       static_cast<ggml_numa_strategy>(get_option<uint32_t>(options, "numa", 0));
+  params.main_gpu = get_option<int32_t>(options, "main_gpu", 0);
 
   llama_backend_init();
   llama_numa_init(params.numa);
