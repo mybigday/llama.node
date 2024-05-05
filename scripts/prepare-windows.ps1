1b226c6d41fc8309028f20df6263410c9e4ffbd9
Hans <hans.chen@bricks.tools>
2024-05-05 22:15:44 +0800

    chore: build CLBlast from source for Windows x64

--- scripts/prepare-windows.ps1
+++ scripts/prepare-windows.ps1
@@ -1,34 +1,8 @@
 $ErrorActionPreference='Stop'
 
-New-Item -ItemType Directory -Force -Path "externals/win32-x64/SDK" | Out-Null
-New-Item -ItemType Directory -Force -Path "externals/win32-arm64" | Out-Null
+New-Item -ItemType Directory -Force -Path "externals" | Out-Null
 
-# x64
-
-cd "externals/win32-x64"
-
-# OpenCL-SDK
-if (-not (Test-Path "OpenCL-SDK.zip")) {
-  Invoke-WebRequest -Uri "https://github.com/KhronosGroup/OpenCL-SDK/releases/download/v2023.12.14/OpenCL-SDK-v2023.12.14-Win-x64.zip" -OutFile "OpenCL-SDK.zip"
-  7z x OpenCL-SDK.zip
-  Copy-Item -Recurse -Force OpenCL-SDK-*/* SDK
-  Remove-Item -Recurse -Force OpenCL-SDK-*
-}
-
-# CLBlast
-if (-not (Test-Path "CLBlast.zip")) {
-  Invoke-WebRequest -Uri "https://github.com/CNugteren/CLBlast/releases/download/1.6.2/CLBlast-1.6.2-windows-x64.zip" -OutFile "CLBlast.zip"
-  7z x CLBlast.zip
-  7z x CLBlast*.7z
-  Copy-Item -Recurse -Force CLBlast-*/* SDK
-  Remove-Item -Recurse -Force CLBlast-*
-}
-
-cd ".."
-
-# ARM64
-
-cd "win32-arm64"
+cd "externals"
 
 if (-not (Test-Path "OpenCL-SDK-source")) {
   git clone "https://github.com/KhronosGroup/OpenCL-SDK.git" "OpenCL-SDK-source" --recursive
@@ -44,16 +18,40 @@ if (-not (Test-Path "CLBlast-source")) {
   cd ".."
 }
 
-if (-not (Test-Path "SDK")) {
-  New-Item -ItemType Directory -Force -Path "SDK" | Out-Null
-  $SDK_ROOT = (Resolve-Path "SDK").Path
+# x64
+
+if (-not (Test-Path "win32-x64/SDK")) {
+  New-Item -ItemType Directory -Force -Path "win32-x64/SDK" | Out-Null
+  $SDK_ROOT = (Resolve-Path "win32-x64/SDK").Path
   # build from source
   cd "OpenCL-SDK-source"
+  rm build -r -fo
+  cmake -B build -DBUILD_DOCS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DOPENCL_SDK_BUILD_SAMPLES=OFF -DOPENCL_SDK_TEST_SAMPLES=OFF -G "Visual Studio 17 2022"
+  cmake --build build --config Release
+  cmake --install build --prefix $SDK_ROOT
+  cd "../CLBlast-source"
+  rm build -r -fo
+  cmake -B build -DBUILD_SHARED_LIBS=ON -DOVERRIDE_MSVC_FLAGS_TO_MT=OFF -DTUNERS=OFF -DOPENCL_ROOT="$SDK_ROOT" -G "Visual Studio 17 2022"
+  cmake --build build --config Release
+  cmake --install build --prefix $SDK_ROOT
+  cd ".."
+}
+
+# ARM64
+
+if (-not (Test-Path "win32-arm64/SDK")) {
+  New-Item -ItemType Directory -Force -Path "win32-arm64/SDK" | Out-Null
+  $SDK_ROOT = (Resolve-Path "win32-arm64/SDK").Path
+  # build from source
+  cd "OpenCL-SDK-source"
+  rm build -r -fo
   cmake -B build -DBUILD_DOCS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DOPENCL_SDK_BUILD_SAMPLES=OFF -DOPENCL_SDK_TEST_SAMPLES=OFF -G "Visual Studio 17 2022" -A arm64
   cmake --build build --config Release
   cmake --install build --prefix $SDK_ROOT
   cd "../CLBlast-source"
+  rm build -r -fo
   cmake -B build -DBUILD_SHARED_LIBS=ON -DOVERRIDE_MSVC_FLAGS_TO_MT=OFF -DTUNERS=OFF -DOPENCL_ROOT="$SDK_ROOT" -G "Visual Studio 17 2022" -A arm64
   cmake --build build --config Release
   cmake --install build --prefix $SDK_ROOT
+  cd ".."
 }
