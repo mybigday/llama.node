00dcefebd35bf79ab5eda40e61bdf272e36df4e4
Hans <hans.chen@bricks.tools>
2024-05-14 22:58:56 +0800

    fix: fix Windows Vulkan build

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -7,5 +7,5 @@ yarn clean ; yarn build-native -a arm64
 
 # Vulkan, might crash on some senerios
 
-yarn clean ; yarn build-native -a x86_64 --CDVULKAN_SDK=(Resolve-Path 'externals/win32-x64/Vulkan-SDK') --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
-yarn clean ; yarn build-native -a arm64 --CDVULKAN_SDK=(Resolve-Path 'externals/win32-arm64/Vulkan-SDK') --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
+yarn clean ; yarn build-native -a x86_64 --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-x64/Vulkan-SDK')" --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
+yarn clean ; yarn build-native -a arm64 --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-arm64/Vulkan-SDK')" --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
