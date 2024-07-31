8967d9bd0b16ccee07ff350c7944121b21a55786
Hans <hans.chen@bricks.tools>
2024-07-31 21:57:54 +0800

    chore: fix typo

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference='Stop'
 yarn clean ; yarn build-native -a x86_64
 yarn clean ; yarn build-native -a arm64
 
-# Vulkan, might crash on some senerios
+# Vulkan, might crash on some scenario
 
 yarn clean ; yarn build-native -a x86_64 --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-x64/Vulkan-SDK')" --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
 yarn clean ; yarn build-native -a arm64 --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-arm64/Vulkan-SDK')" --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
