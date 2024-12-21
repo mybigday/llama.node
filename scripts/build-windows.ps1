7683d4037b0c62f7a3d4e0d1a920286b3c303984
Jhen-Jie Hong <developer@jhen.me>
2024-12-21 10:42:56 +0800

    fix: try to fix windows build

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -3,9 +3,9 @@ $ErrorActionPreference='Stop'
 # General
 
 yarn clean ; yarn build-native -a x86_64
-yarn clean ; yarn build-native -a arm64
+yarn clean ; yarn build-native -a arm64 --CDCMAKE_C_COMPILER=clang --CDCMAKE_CXX_COMPILER=clang++ 
 
 # Vulkan, might crash on some scenario
 
 yarn clean ; yarn build-native -a x86_64 --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-x64/Vulkan-SDK')" --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
-yarn clean ; yarn build-native -a arm64 --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-arm64/Vulkan-SDK')" --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
+yarn clean ; yarn build-native -a arm64 --CDCMAKE_C_COMPILER=clang --CDCMAKE_CXX_COMPILER=clang++ --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-arm64/Vulkan-SDK')" --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
