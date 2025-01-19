f084d66314b7ff88bf66d94ffe438f0e22773713
Jhen-Jie Hong <developer@jhen.me>
2025-01-19 18:25:25 +0800

    feat: build cuda bin for linux (x86/arm) (#77)
    * ci: use new linux arm runner for build build-linux-arm64
    
    * ci: install yarn by self
    
    * ci: enable linux cuda build
    
    * feat: add CMAKE_CUDA_ARCHITECTURES
    
    * feat: update scripts/prepare-linux.sh
    
    * feat: set GGML_NATIVE=OFF

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -9,7 +9,28 @@ ARCH=${ARCH:-${1:-$(uname -m)}}
 if [ $ARCH == "x86_64" ]; then
   yarn clean && yarn build-native
   yarn clean && yarn build-native --CDLLAMA_VULKAN=1 --CDVARIANT=vulkan
+
+  # Check CUDA is available
+  if [ -f /usr/local/cuda/bin/nvcc ]; then
+    yarn clean && yarn build-native \
+      --CDLLAMA_CUDA=1 \
+      --CDVARIANT=cuda \
+      --CDCMAKE_CUDA_ARCHITECTURES=89 # > GeForce RTX 40 series
+  else
+    echo "CUDA is not available, skipping CUDA build"
+  fi
 else
   yarn clean && yarn build-native --CDGGML_NATIVE=OFF
   yarn clean && yarn build-native --CDGGML_NATIVE=OFF --CDLLAMA_VULKAN=1 --CDVULKAN_SDK="$(realpath 'externals/arm64-Vulkan-SDK')" --CDVARIANT=vulkan
+
+  # Check CUDA is available
+  if [ -f /usr/local/cuda/bin/nvcc ]; then
+    yarn clean && yarn build-native \
+      --CDLLAMA_CUDA=1 \
+      --CDVARIANT=cuda \
+      --CDGGML_NATIVE=OFF \
+      --CDCMAKE_CUDA_ARCHITECTURES=87 # > Jetson Orin series
+  else
+    echo "CUDA is not available, skipping CUDA build"
+  fi
 fi
