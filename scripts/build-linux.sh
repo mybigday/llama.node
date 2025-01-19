3c12e9a0e44dbc132abeafa42d15603c25eb7170
Jhen-Jie Hong <developer@jhen.me>
2025-01-19 11:06:43 +0800

    ci: enable linux cuda build

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -9,7 +9,21 @@ ARCH=${ARCH:-${1:-$(uname -m)}}
 if [ $ARCH == "x86_64" ]; then
   yarn clean && yarn build-native
   yarn clean && yarn build-native --CDLLAMA_VULKAN=1 --CDVARIANT=vulkan
+
+  # Check CUDA is available
+  if [ -f /usr/local/cuda/bin/nvcc ]; then
+    yarn clean && yarn build-native --CDLLAMA_CUDA=1 --CDVARIANT=cuda
+  else
+    echo "CUDA is not available, skipping CUDA build"
+  fi
 else
   yarn clean && yarn build-native
   yarn clean && yarn build-native --CDLLAMA_VULKAN=1 --CDVULKAN_SDK="$(realpath 'externals/arm64-Vulkan-SDK')" --CDVARIANT=vulkan
+
+  # Check CUDA is available
+  if [ -f /usr/local/cuda/bin/nvcc ]; then
+    yarn clean && yarn build-native --CDLLAMA_CUDA=1 --CDVARIANT=cuda
+  else
+    echo "CUDA is not available, skipping CUDA build"
+  fi
 fi
