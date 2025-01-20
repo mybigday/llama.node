639608358028e2019db0d582401eb9c3e321c18a
Jhen-Jie Hong <developer@jhen.me>
2025-01-20 13:57:19 +0800

    feat: add static model info method (#76)
    * feat(cpp): add static model info method
    
    * fix(cpp): include llama-impl.h
    
    * feat: impl loadLlamaModelInfo

--- src/LlamaContext.h
+++ src/LlamaContext.h
@@ -5,6 +5,7 @@ class LlamaCompletionWorker;
 class LlamaContext : public Napi::ObjectWrap<LlamaContext> {
 public:
   LlamaContext(const Napi::CallbackInfo &info);
+  static Napi::Value ModelInfo(const Napi::CallbackInfo& info);
   static void Init(Napi::Env env, Napi::Object &exports);
 
 private:
