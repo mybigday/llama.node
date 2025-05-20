bc63e8961bf38ea1eb4e8e7eb9fb7fa18f4ec2a9
Jhen-Jie Hong <developer@jhen.me>
2025-05-20 12:05:26 +0800

    fix(mtmd): state load and save

--- src/LoadSessionWorker.cpp
+++ src/LoadSessionWorker.cpp
@@ -11,6 +11,13 @@ void LoadSessionWorker::Execute() {
   // reserve the maximum number of tokens for capacity
   std::vector<llama_token> tokens;
   tokens.reserve(_sess->params().n_ctx);
+
+  // Find LLAMA_TOKEN_NULL in the tokens and resize the array to the index of the null token
+  auto null_token_iter = std::find(tokens.begin(), tokens.end(), LLAMA_TOKEN_NULL);
+  if (null_token_iter != tokens.end()) {
+    tokens.resize(std::distance(tokens.begin(), null_token_iter));
+  }
+
   if (!llama_state_load_file(_sess->context(), _path.c_str(), tokens.data(),
                              tokens.capacity(), &count)) {
     SetError("Failed to load session");
--- src/SaveSessionWorker.cpp
+++ src/SaveSessionWorker.cpp
@@ -9,6 +9,13 @@ SaveSessionWorker::SaveSessionWorker(const Napi::CallbackInfo &info,
 void SaveSessionWorker::Execute() {
   _sess->get_mutex().lock();
   auto tokens = _sess->tokens_ptr();
+
+  // Find LLAMA_TOKEN_NULL in the tokens and resize the array to the index of the null token
+  auto null_token_iter = std::find(tokens->begin(), tokens->end(), LLAMA_TOKEN_NULL);
+  if (null_token_iter != tokens->end()) {
+    tokens->resize(std::distance(tokens->begin(), null_token_iter));
+  }
+
   if (!llama_state_save_file(_sess->context(), _path.c_str(), tokens->data(),
                              tokens->size())) {
     SetError("Failed to save session");
