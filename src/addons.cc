0f43b2d18903a5e1074deafd1e147e833d887e86
Jhen-Jie Hong <developer@jhen.me>
2025-05-21 11:39:51 +0800

    fix(cpp): thread safe issue of toggle native logging (#93)
    * fix(cpp): thread safe issue of toggle native logging
    
    * fix(ts): update test
    
    * fix(cpp): remove cleanup on release content

--- src/addons.cc
+++ src/addons.cc
@@ -1,8 +1,29 @@
 #include "LlamaContext.h"
 #include <napi.h>
 
+// Forward declaration of our cleanup function
+extern "C" void cleanup_logging();
+
+// Register cleanup function on module unload
+static Napi::Value register_cleanup(const Napi::CallbackInfo& info) {
+  napi_add_env_cleanup_hook(info.Env(), [](void*) {
+    cleanup_logging();
+  }, nullptr);
+  
+  return info.Env().Undefined();
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   LlamaContext::Init(env, exports);
+  
+  // Register our cleanup handler for module unload
+  exports.Set("__registerCleanup", Napi::Function::New(env, register_cleanup));
+  
+  // Also register cleanup directly on module init
+  napi_add_env_cleanup_hook(env, [](void*) {
+    cleanup_logging();
+  }, nullptr);
+  
   return exports;
 }
 
