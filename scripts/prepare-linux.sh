1a10e90c6e68755a47e5b4c2750f3b44e0f1997e
Hans <hans.chen@bricks.tools>
2025-06-10 20:02:17 +0800

    ci: remove unused script

--- scripts/prepare-linux.sh
+++ scripts/prepare-linux.sh
@@ -26,23 +26,3 @@ done
 
 run_as_root apt-get update
 run_as_root apt-get install -qy lsb-release wget llvm clang lld cmake ninja-build libomp-dev ccache
-
-if [ "$TARGET" == "vulkan" ] && ! command -v glslc &> /dev/null; then
-  if ! run_as_root apt-get install -qy shaderc; then
-    # build from source
-    git clone https://github.com/google/shaderc.git --depth 1
-    pushd shaderc
-    git clone https://github.com/KhronosGroup/SPIRV-Tools.git --depth 1 --branch vulkan-sdk-1.4.313 third_party/spirv-tools
-    git clone https://github.com/KhronosGroup/SPIRV-Headers.git --depth 1 --branch vulkan-sdk-1.4.313 third_party/spirv-headers
-    ln -s third_party/spirv-headers third_party/spirv-tools/external/spirv-headers
-    cmake -B build -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=/usr \
-      -DSHADERC_SKIP_TESTS=ON \
-      -DSHADERC_SKIP_EXAMPLES=ON \
-      -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-      -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-    cmake --build build
-    run_as_root cmake --install build
-    popd
-  fi
-fi
