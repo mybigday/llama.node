fdc8aef582312357bfc20b22242a16eaecbae338
Hans <hans.chen@bricks.tools>
2025-06-10 15:24:56 +0800

    ci: change to use vulkan official  SDK installer

--- scripts/prepare-windows.ps1
+++ scripts/prepare-windows.ps1
@@ -4,83 +4,38 @@ param (
 
 $ErrorActionPreference='Stop'
 
-function DelIfExist {
-  param([string]$path)
-  if (Test-Path $path) {
-    Remove-Item $path -Recurse -Force
-  }
+# check processor architecture
+$Arch = [System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture
+
+if ($Arch -eq "X64") {
+  $VULKAN_URL = "https://sdk.lunarg.com/sdk/download/1.4.313.1/windows/vulkansdk-windows-X64-1.4.313.1.exe"
+} else {
+  $VULKAN_URL = "https://sdk.lunarg.com/sdk/download/1.4.313.1/windows/vulkansdk-windows-ARM64-1.4.313.1.exe"
 }
 
-New-Item -ItemType Directory -Force -Path "externals" | Out-Null
+$VULKAN_COMPONENTS = "com.lunarg.vulkan.sdl2 com.lunarg.vulkan.glm com.lunarg.vulkan.volk com.lunarg.vulkan.vma com.lunarg.vulkan.debug"
 
-cd "externals"
-
-if (-not (Test-Path "win32-x64/Vulkan-SDK") -or -not (Test-Path "win32-arm64/Vulkan-SDK")) {
-  if (-not (Test-Path "Vulkan-Headers-source")) {
-    git clone "https://github.com/KhronosGroup/Vulkan-Headers.git" "Vulkan-Headers-source"
-    cd "Vulkan-Headers-source"
-    git checkout sdk-1.3.261
-    cd ".."
-  }
-  if (-not (Test-Path "Vulkan-Loader-source")) {
-    git clone "https://github.com/KhronosGroup/Vulkan-Loader.git" "Vulkan-Loader-source"
-    cd "Vulkan-Loader-source"
-    git checkout sdk-1.3.261
-    cd ".."
-  }
-  if (-not (Test-Path "Vulkan-Hpp-source")) {
-    git clone "https://github.com/KhronosGroup/Vulkan-Hpp.git" "Vulkan-Hpp-source"
-    cd "Vulkan-Hpp-source"
-    git checkout v1.3.261
-    git submodule update --init --recursive
-    cd ".."
-  }
+if ($Arch -eq "X64" -and ($target -eq "all" -or $target -eq "arm64")) {
+  $VULKAN_COMPONENTS += " com.lunarg.vulkan.arm64"
 }
 
-# x64
-
-if (-not (Test-Path "win32-x64/Vulkan-SDK") -and ($target -eq "all" -or $target -eq "x86_64")) {
-  New-Item -ItemType Directory -Force -Path "win32-x64/Vulkan-SDK" | Out-Null
-  $VULKAN_ROOT = (Resolve-Path "win32-x64/Vulkan-SDK").Path
-  # build from source
-  cd "Vulkan-Headers-source"
-  DelIfExist "build"
-  cmake -B build -G "Visual Studio 17 2022" -A x64
-  cmake --build build --config Release
-  cmake --install build --prefix $VULKAN_ROOT
-  cd "../Vulkan-Loader-source"
-  DelIfExist "build"
-  cmake -B build -G "Visual Studio 17 2022" -DVULKAN_HEADERS_INSTALL_DIR="$VULKAN_ROOT" -DUSE_MASM=OFF -A x64
-  cmake --build build --config Release
-  cmake --install build --prefix $VULKAN_ROOT
-  cd "../Vulkan-Hpp-source"
-  DelIfExist "build"
-  cmake -B build -G "Visual Studio 17 2022" -DVULKAN_HPP_INSTALL=ON -DVULKAN_HPP_RUN_GENERATOR=ON
-  cmake --build build --config Release
-  cmake --install build --prefix $VULKAN_ROOT
-  cd ".."
+if ($Arch -eq "Arm64" -and ($target -eq "all" -or $target -eq "x86_64")) {
+  $VULKAN_COMPONENTS += " com.lunarg.vulkan.x64"
 }
 
-# ARM64
+# install vulkan sdk
+if ($env:VULKAN_SDK -eq $null) {
+  wget $VULKAN_URL -O "vulkansdk.exe"
+  .\vulkansdk.exe --accept-licenses --default-answer --confirm-command install $VULKAN_COMPONENTS
+  rm vulkansdk.exe
 
-if (-not (Test-Path "win32-arm64/Vulkan-SDK") -and ($target -eq "all" -or $target -eq "arm64")) {
-  New-Item -ItemType Directory -Force -Path "win32-arm64/Vulkan-SDK" | Out-Null
-  $VULKAN_ROOT = (Resolve-Path "win32-arm64/Vulkan-SDK").Path
-  # build from source
-  cd "Vulkan-Headers-source"
-  DelIfExist "build"
-  cmake -B build -G "Visual Studio 17 2022" -A arm64
-  cmake --build build --config Release
-  cmake --install build --prefix $VULKAN_ROOT
-  cd "../Vulkan-Loader-source"
-  DelIfExist "build"
-  cmake -B build -G "Visual Studio 17 2022" -DVULKAN_HEADERS_INSTALL_DIR="$VULKAN_ROOT" -DUSE_MASM=OFF -A arm64
-  cmake --build build --config Release
-  cmake --install build --prefix $VULKAN_ROOT
-  cd "../Vulkan-Hpp-source"
-  DelIfExist "build"
-  cmake -B build -G "Visual Studio 17 2022" -DVULKAN_HPP_INSTALL=ON -DVULKAN_HPP_RUN_GENERATOR=ON
-  cmake --build build --config Release
-  cmake --install build --prefix $VULKAN_ROOT
-  cd ".."
+  $env:VK_SDK_PATH = "C:\VulkanSDK\1.4.313.1"
+  $env:VULKAN_SDK = "C:\VulkanSDK\1.4.313.1"
+  $env:PATH = "C:\VulkanSDK\1.4.313.1\Bin;$env:PATH"
+
+  if ($env:GITHUB_ENV -ne $null) {
+    Add-Content -Path $env:GITHUB_ENV -Value "VULKAN_SDK=$env:VULKAN_SDK" -Append
+    Add-Content -Path $env:GITHUB_ENV -Value "VK_SDK_PATH=$env:VK_SDK_PATH" -Append
+  }
+}
 }
