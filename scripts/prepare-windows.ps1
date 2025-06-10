792c876f6e26ae90fd992a8319008ced2b40d36a
Hans <hans.chen@bricks.tools>
2025-06-10 17:16:17 +0800

    ci: use `jakoch/install-vulkan-sdk-action` to setup vulkan

--- scripts/prepare-windows.ps1
+++ scripts/prepare-windows.ps1
@@ -13,41 +13,6 @@ if ($arch -eq "native") {
 
 $ErrorActionPreference='Stop'
 
-# check processor architecture
-$Arch = [System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture
-
-if ($Arch -eq "X64") {
-  $VULKAN_URL = "https://sdk.lunarg.com/sdk/download/1.4.313.1/windows/vulkansdk-windows-X64-1.4.313.1.exe"
-} else {
-  $VULKAN_URL = "https://sdk.lunarg.com/sdk/download/1.4.313.1/warm/vulkansdk-windows-ARM64-1.4.313.1.exe"
-}
-
-$VULKAN_COMPONENTS = "com.lunarg.vulkan.sdl2 com.lunarg.vulkan.glm com.lunarg.vulkan.volk com.lunarg.vulkan.vma com.lunarg.vulkan.debug"
-
-if ($Arch -eq "X64" -and ($arch -eq "all" -or $arch -eq "arm64")) {
-  $VULKAN_COMPONENTS += " com.lunarg.vulkan.arm64"
-}
-
-if ($Arch -eq "Arm64" -and ($arch -eq "all" -or $arch -eq "x64")) {
-  $VULKAN_COMPONENTS += " com.lunarg.vulkan.x64"
-}
-
-# install vulkan sdk
-if ($env:VULKAN_SDK -eq $null -and ($target -eq "all" -or $target -eq "vulkan")) {
-  Invoke-WebRequest $VULKAN_URL -OutFile "vulkansdk.exe"
-  .\vulkansdk.exe --accept-licenses --default-answer --confirm-command install $VULKAN_COMPONENTS
-  rm vulkansdk.exe
-
-  $env:VK_SDK_PATH = "C:\VulkanSDK\1.4.313.1"
-  $env:VULKAN_SDK = "C:\VulkanSDK\1.4.313.1"
-  $env:PATH = "C:\VulkanSDK\1.4.313.1\Bin;$env:PATH"
-
-  if ($env:GITHUB_ENV -ne $null) {
-    Add-Content -Path $env:GITHUB_ENV -Value "VULKAN_SDK=$env:VULKAN_SDK"
-    Add-Content -Path $env:GITHUB_ENV -Value "VK_SDK_PATH=$env:VK_SDK_PATH"
-  }
-}
-
 # install CUDA
 if (($arch -eq "all" -or $arch -eq "x64") -and $env:CUDA_PATH -eq $null -and ($target -eq "all" -or $target -eq "cuda")) {
   choco install cuda --version=12.9.1.576 -y
