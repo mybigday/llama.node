2da6bfb3fb9dc296933d1adcd1614218d675223f
Hans <hans.chen@bricks.tools>
2025-06-10 18:44:00 +0800

    fix: fix build from source

--- scripts/prepare-linux.sh
+++ scripts/prepare-linux.sh
@@ -30,9 +30,26 @@ run_as_root apt-get install -qy lsb-release wget llvm clang lld cmake ninja-buil
 if [ "$TARGET" == "vulkan" ] && ! command -v glslc &> /dev/null; then
   if ! run_as_root apt-get install -qy shaderc; then
     # build from source
+    # SPIRV-Tools
+    git clone https://github.com/KhronosGroup/SPIRV-Tools.git --depth 1
+    pushd SPIRV-Tools
+    cmake -B build -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=/usr \
+      -DSPIRV_SKIP_TESTS=ON \
+      -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+      -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+    cmake --build build
+    run_as_root cmake --install build
+    popd
+    # shaderc
     git clone https://github.com/google/shaderc.git --depth 1
     pushd shaderc
-    cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DSHADERC_SKIP_TESTS=ON -DSHADERC_SKIP_EXAMPLES=ON
+    cmake -B build -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=/usr \
+      -DSHADERC_SKIP_TESTS=ON \
+      -DSHADERC_SKIP_EXAMPLES=ON \
+      -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+      -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
     cmake --build build
     run_as_root cmake --install build
     popd
