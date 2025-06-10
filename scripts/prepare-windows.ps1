b16f3d67dde1dcc2316ae36085d547b7c8772de8
Hans <hans.chen@bricks.tools>
2025-06-10 23:53:59 +0800

    fix: use mingw-llvm to build

--- scripts/prepare-windows.ps1
+++ scripts/prepare-windows.ps1
@@ -1,17 +1,20 @@
 param (
   [string]$arch = "native",
-  [string]$target = "default"
+  [string]$target = "default",
+  [string]$toolchain = "clang-cl"
 )
 
+$ErrorActionPreference='Stop'
+
+$nativeArch = [System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture
+
 if ($arch -eq "native") {
-  if ([System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture -eq "Arm64") {
+  if ($nativeArch -eq "Arm64") {
     $arch = "arm64"
   } else {
     $arch = "x64"
   }
 }
-
-$ErrorActionPreference='Stop'
 
 # install CUDA
 if (($arch -eq "all" -or $arch -eq "x64") -and $env:CUDA_PATH -eq $null -and ($target -eq "all" -or $target -eq "cuda")) {
@@ -27,10 +30,22 @@ if (($arch -eq "all" -or $arch -eq "x64") -and $env:CUDA_PATH -eq $null -and ($t
   }
 }
 
-if ($env:GITHUB_ENV -ne $null) {
-  Add-Content -Path $env:GITHUB_ENV -Value "PATH=$env:PATH"
-}
-
 if (Get-Command ccache -ErrorAction SilentlyContinue) {
   choco install ccache -y
+}
+
+if ($toolchain -eq "mingw-clang" -and (Get-Command clang -ErrorAction SilentlyContinue) -eq $null) {
+  $version = "20250528"
+  if ($nativeArch -eq "X64") {
+    $name = "llvm-mingw-${version}-ucrt-x86_64"
+  } elseif ($nativeArch -eq "Arm64") {
+    $name = "llvm-mingw-${version}-ucrt-aarch64"
+  }
+  Invoke-WebRequest -Uri "https://github.com/mstorsjo/llvm-mingw/releases/download/${version}/${name}.zip" -OutFile "llvm-mingw.zip"
+  Expand-Archive -Path "llvm-mingw.zip"
+  $env:PATH += ";$(Resolve-Path $name\bin)"
+}
+
+if ($env:GITHUB_ENV -ne $null) {
+  Add-Content -Path $env:GITHUB_ENV -Value "PATH=$env:PATH"
 }
