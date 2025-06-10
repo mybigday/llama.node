abbb85681662c3662b4399c383037824a4ea428e
Hans <hans.chen@bricks.tools>
2025-06-10 19:39:00 +0800

    fix: fix build from source

--- scripts/prepare-linux.sh
+++ scripts/prepare-linux.sh
@@ -30,21 +30,11 @@ run_as_root apt-get install -qy lsb-release wget llvm clang lld cmake ninja-buil
 if [ "$TARGET" == "vulkan" ] && ! command -v glslc &> /dev/null; then
   if ! run_as_root apt-get install -qy shaderc; then
     # build from source
-    # SPIRV-Tools
-    git clone https://github.com/KhronosGroup/SPIRV-Tools.git --depth 1 --branch vulkan-sdk-1.4.313
-    pushd SPIRV-Tools
-    git clone https://github.com/KhronosGroup/SPIRV-Headers.git --depth 1 --branch vulkan-sdk-1.4.313 external/spirv-headers
-    cmake -B build -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=/usr \
-      -DSPIRV_SKIP_TESTS=ON \
-      -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-      -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-    cmake --build build
-    run_as_root cmake --install build
-    popd
-    # shaderc
     git clone https://github.com/google/shaderc.git --depth 1
     pushd shaderc
+    git clone https://github.com/KhronosGroup/SPIRV-Tools.git --depth 1 --branch vulkan-sdk-1.4.313 third_party/spirv-tools
+    git clone https://github.com/KhronosGroup/SPIRV-Headers.git --depth 1 --branch vulkan-sdk-1.4.313 third_party/spirv-headers
+    ln -s third_party/spirv-headers third_party/spirv-tools/external/spirv-headers
     cmake -B build -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=/usr \
       -DSHADERC_SKIP_TESTS=ON \
