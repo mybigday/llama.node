5e11a046de47e67664a920909114907b86db0944
Hans <hans.chen@bricks.tools>
2025-06-10 13:12:05 +0800

    fix: use latest build flags

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -8,12 +8,12 @@ ARCH=${ARCH:-${1:-$(uname -m)}}
 
 if [ $ARCH == "x86_64" ]; then
   npx cmake-js rebuild -C --CDTO_PACKAGE=ON
-  npx cmake-js rebuild -C --CDTO_PACKAGE=ON --CDLLAMA_VULKAN=1 --CDVARIANT=vulkan
+  npx cmake-js rebuild -C --CDTO_PACKAGE=ON --CDGGML_VULKAN=1 --CDVARIANT=vulkan
 
   # Check CUDA is available
   if [ -f /usr/local/cuda/bin/nvcc ]; then
     npx cmake-js rebuild -C --CDTO_PACKAGE=ON \
-      --CDLLAMA_CUDA=1 \
+      --CDGGML_CUDA=1 \
       --CDVARIANT=cuda \
       --CDCMAKE_CUDA_ARCHITECTURES=89 # > GeForce RTX 40 series
   else
@@ -23,14 +23,14 @@ else
   npx cmake-js rebuild -C --CDTO_PACKAGE=ON --CDGGML_NATIVE=OFF
   npx cmake-js rebuild -C --CDTO_PACKAGE=ON \
     --CDGGML_NATIVE=OFF \
-    --CDLLAMA_VULKAN=1 \
+    --CDGGML_VULKAN=1 \
     --CDVULKAN_SDK="$(realpath 'externals/arm64-Vulkan-SDK')" \
     --CDVARIANT=vulkan
 
   # Check CUDA is available
   if [ -f /usr/local/cuda/bin/nvcc ]; then
     npx cmake-js rebuild -C --CDTO_PACKAGE=ON \
-      --CDLLAMA_CUDA=1 \
+      --CDGGML_CUDA=1 \
       --CDVARIANT=cuda \
       --CDGGML_NATIVE=OFF \
       --CDCMAKE_CUDA_ARCHITECTURES=87 # > Jetson Orin series
--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -25,15 +25,24 @@ if ($target -eq "all" -or $target -eq "arm64") {
 # Vulkan, might crash on some scenario
 
 if ($target -eq "all" -or $target -eq "x86_64") {
-  npx cmake-js rebuild -C -a x86_64 --CDTO_PACKAGE=ON --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-x64/Vulkan-SDK')" --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
+  npx cmake-js rebuild -C -a x86_64 --CDTO_PACKAGE=ON $x86Args `
+    --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-x64/Vulkan-SDK')" `
+    --CDVARIANT=vulkan `
+    --CDGGML_VULKAN=1
 }
 if ($target -eq "all" -or $target -eq "arm64") {
-  npx cmake-js rebuild -C -a arm64 --CDTO_PACKAGE=ON --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-arm64/Vulkan-SDK')" --CDVARIANT=vulkan --CDLLAMA_VULKAN=1
+  npx cmake-js rebuild -C -a arm64 --CDTO_PACKAGE=ON $arm64Args `
+    --CDVULKAN_SDK="$(Resolve-Path 'externals/win32-arm64/Vulkan-SDK')" `
+    --CDVARIANT=vulkan `
+    --CDGGML_VULKAN=1
 }
 
 # CUDA
 
 if ($target -eq "all" -or $target -eq "x86_64") {
-  # See: https://developer.nvidia.com/cuda-gpus
-  npx cmake-js rebuild -C -a x86_64 --CDTO_PACKAGE=ON --CDVARIANT=cuda --CDLLAMA_CUDA=1 --CDCMAKE_CUDA_ARCHITECTURES="75;80;86;89;120"
+  npx cmake-js rebuild -C -a x86_64 --CDTO_PACKAGE=ON $x86Args `
+    --CDVARIANT=cuda `
+    --CDGGML_CUDA=1 `
+    --CDGGML_CUDA_F16=1 `
+    --CDCMAKE_CUDA_ARCHITECTURES="75;80;86;89;120" # See: https://developer.nvidia.com/cuda-gpus
 }
