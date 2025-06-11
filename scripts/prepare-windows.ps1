dedf2c20d4dcc6bf204a81c63b52709fd032e838
Hans <hans.chen@bricks.tools>
2025-06-11 05:31:31 +0000

    ci: use source, fix path

--- scripts/prepare-windows.ps1
+++ scripts/prepare-windows.ps1
@@ -19,8 +19,8 @@ if ($arch -eq "native") {
 # install CUDA
 if (($arch -eq "all" -or $arch -eq "x64") -and $env:CUDA_PATH -eq $null -and ($target -eq "all" -or $target -eq "cuda")) {
   choco install cuda --version=12.9.1.576 -y
-  $env:PATH += ';C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9\bin'
-  $env:PATH += ';C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9\libnvvp'
+  $env:Path += ';C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9\bin'
+  $env:Path += ';C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9\libnvvp'
   $env:CUDA_PATH = 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9'
   $env:CUDA_PATH_V12_9 = 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9'
 
@@ -43,16 +43,11 @@ if ($toolchain -eq "mingw-clang") {
   }
   Invoke-WebRequest -Uri "https://github.com/mstorsjo/llvm-mingw/releases/download/${version}/${name}.zip" -OutFile "llvm-mingw.zip"
   Expand-Archive -Path "llvm-mingw.zip" -DestinationPath .
-  $env:PATH += ";$(Resolve-Path $name\bin)"
-  [System.Environment]::SetEnvironmentVariable(
-    "PATH",
-    "$(Resolve-Path $name\bin);$([System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::User))",
-    [System.EnvironmentVariableTarget]::User
-  )
+  $env:Path += ";$(Resolve-Path $name\bin)"
 
   choco install ninja -y
 }
 
 if ($env:GITHUB_ENV -ne $null) {
-  Add-Content -Path $env:GITHUB_ENV -Value "PATH=$env:PATH"
+  Add-Content -Path $env:GITHUB_ENV -Value "PATH=$env:Path"
 }
