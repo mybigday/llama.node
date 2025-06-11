baff6cc3bd4b553b6a4503d144e89601be1a22da
Hans <hans.chen@bricks.tools>
2025-06-11 16:40:23 +0800

    refactor: use refreshenv

--- scripts/prepare-windows.ps1
+++ scripts/prepare-windows.ps1
@@ -19,15 +19,12 @@ if ($arch -eq "native") {
 # install CUDA
 if (($arch -eq "all" -or $arch -eq "x64") -and $env:CUDA_PATH -eq $null -and ($target -eq "all" -or $target -eq "cuda")) {
   choco install cuda --version=12.9.1.576 -y
-  $env:Path += ';C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9\bin'
-  $env:Path += ';C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9\libnvvp'
-  $env:CUDA_PATH = 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9'
-  $env:CUDA_PATH_V12_9 = 'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9'
 
-  if ($env:GITHUB_ENV -ne $null) {
-    Add-Content -Path $env:GITHUB_ENV -Value "CUDA_PATH=$env:CUDA_PATH"
-    Add-Content -Path $env:GITHUB_ENV -Value "CUDA_PATH_V12_9=$env:CUDA_PATH_V12_9"
+  if ($env:ChocolateyInstall -eq $null) {
+    $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
   }
+  Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+  refreshenv
 }
 
 if (Get-Command ccache -ErrorAction SilentlyContinue) {
