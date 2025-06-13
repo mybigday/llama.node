7d4ce6f536ca4c45358c593301d3ca35a9d912ed
Hans <hans.chen@bricks.tools>
2025-06-13 22:53:44 +0800

    fix: linux prebuild use libgomp

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -20,17 +20,18 @@ done
 if [ $ARCH == "x86_64" ]; then
   # default
   if [ $TARGET == "all" ] || [ $TARGET == "default" ]; then
-    npx cmake-js rebuild -C --CDTO_PACKAGE=ON
+    npx cmake-js rebuild -C --CDTO_PACKAGE=ON --CDCLANG_USE_GOMP=ON
   fi
 
   # vulkan
   if [ $TARGET == "all" ] || [ $TARGET == "vulkan" ]; then
-    npx cmake-js rebuild -C --CDTO_PACKAGE=ON --CDGGML_VULKAN=1 --CDVARIANT=vulkan
+    npx cmake-js rebuild -C --CDTO_PACKAGE=ON  --CDCLANG_USE_GOMP=ON --CDGGML_VULKAN=1 --CDVARIANT=vulkan
   fi
 
   # cuda
   if [ $TARGET == "all" ] || [ $TARGET == "cuda" ]; then
     npx cmake-js rebuild -C --CDTO_PACKAGE=ON \
+      --CDCLANG_USE_GOMP=ON \
       --CDGGML_CUDA=1 \
       --CDVARIANT=cuda \
       --CDCMAKE_CUDA_ARCHITECTURES=89 # > GeForce RTX 40 series
@@ -38,14 +39,15 @@ if [ $ARCH == "x86_64" ]; then
 else
   # default
   if [ $TARGET == "all" ] || [ $TARGET == "default" ]; then
-    npx cmake-js rebuild -C --CDTO_PACKAGE=ON --CDGGML_NATIVE=OFF
+    npx cmake-js rebuild -C --CDTO_PACKAGE=ON  --CDCLANG_USE_GOMP=ON --CDGGML_NATIVE=OFF
   fi
 
   # vulkan
   if [ $TARGET == "all" ] || [ $TARGET == "vulkan" ]; then
     npx cmake-js rebuild -C --CDTO_PACKAGE=ON \
-    --CDGGML_NATIVE=OFF \
-    --CDGGML_VULKAN=1 \
+      --CDCLANG_USE_GOMP=ON \
+      --CDGGML_NATIVE=OFF \
+      --CDGGML_VULKAN=1 \
       --CDVULKAN_SDK="$(realpath 'externals/arm64-Vulkan-SDK')" \
       --CDVARIANT=vulkan
   fi
@@ -53,6 +55,7 @@ else
   # cuda
   if [ $TARGET == "all" ] || [ $TARGET == "cuda" ]; then
     npx cmake-js rebuild -C --CDTO_PACKAGE=ON \
+      --CDCLANG_USE_GOMP=ON \
       --CDGGML_CUDA=1 \
       --CDVARIANT=cuda \
       --CDGGML_NATIVE=OFF \
