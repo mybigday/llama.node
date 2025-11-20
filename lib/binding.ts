083109f4ab835d78e549177e32ac93b139aebe23
Hans <hans.chen@bricks.tools>
2025-11-20 20:30:18 +0800

    feat: make hexagon not need configure LD_LIBRARY_PATH

--- lib/binding.ts
+++ lib/binding.ts
@@ -1,8 +1,3 @@
-import { createRequire } from 'node:module'
-import path from 'node:path'
-
-const _require = require ?? createRequire(path.join(process.cwd(), 'node_modules'))
-
 export type MessagePart = {
   type: string
   text?: string
@@ -599,17 +594,6 @@ const loadPlatformPackage = async (
 }
 
 export const loadModule = async (variant?: LibVariant): Promise<Module> => {
-  try {
-    if (variant === 'snapdragon' && process.platform === 'linux') {
-      const packagePath = path.resolve(_require.resolve(getPlatformPackageName(variant)), '../')
-      if (process.env.LD_LIBRARY_PATH) {
-        process.env.LD_LIBRARY_PATH += `:${packagePath}`
-      } else {
-        process.env.LD_LIBRARY_PATH = packagePath
-      }
-    }
-  } catch {}
-
   let module = await loadPlatformPackage(getPlatformPackageName(variant))
   if (module) {
     return module
